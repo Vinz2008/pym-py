58561132e9e9fb9490c3875989b6564fc2c7d5fc
Vinz2008 <vincentbidarddelanoe@gmail.com>
2022-03-27 12:26:13 +0200

    started packaging for pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+	name="pym",
+	version="0.1",
+	scripts=["pym"],
+	author="Vincent Bidard de la Noë",
+	author_email="vincentbidarddelanoe@gmail.com",
+	description="Python Preprocessor for macros",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/Vinz2008/pym-py",
+	packages=setuptools.find_packages(),
+	classifiers=[
+	         "Programming Language :: Python :: 3",
+	],
+)
+
+
+
